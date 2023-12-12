c9e1c62d8c4e68e39f63b4a896dcc0c6cfca1281
kebrito <kellybritomejia@gmail.com>
2023-12-12 15:23:33 -0500

    Creando app de comandos

--- FT-M3-master/01-Node/homework/01 - Exercises/bash.js
+++ FT-M3-master/01-Node/homework/01 - Exercises/bash.js
@@ -1,11 +1,43 @@
-const process = require('process');
-const { Z_ASCII } = require('zlib');
-const commands = require('./commands/index.js');
+const process = require("process");
+const { Z_ASCII } = require("zlib");
+const commands = require("./commands/index.js");
 
-function bash() {}
+function print(output) {
+  const stringOutput = output.toString();
+  // Convertir a string si no lo es
+  process.stdout.write(stringOutput);
+  process.stdout.write("\nprompt > ");
+  // process.stdout.write(output);
+  // process.stdout.write("\nprompt > ");
+}
 
+function bash() {
+  process.stdout.write("prompt > ");
+
+  // Agregar stdin.on para escuchar lo que el  usuario escribe..
+  process.stdin.on("data", (data) => {
+    //  Convertimos la data a string y le eliminamos los espacios al final y principio.
+
+    let args = data.toString().trim();
+    //  Creamos la variable y guardamos la primera palabra del string (echo)
+    // args -> "echo hola mundo"
+    // args.split -> ["echo", "hola", "mundo"]
+    let [cmd, ...others] = args.split(" ");
+
+    //  Verificamos si el comando no existe en el objeto cmd?
+    if (!commands[cmd]) {
+      print(`command not found: ${cmd}`);
+
+      // Si existe ejecutamos la funcion correspondiente
+    } else {
+      let argString = others.join(" ");
+      commands[cmd](print, argString);
+    }
+  });
+}
 bash();
+
 module.exports = {
-   print,
-   bash,
+  print,
+  bash,
 };
--- FT-M3-master/01-Node/homework/01 - Exercises/commands/index.js
+++ FT-M3-master/01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,77 @@
 const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
+const { error } = require("console");
+const { request } = require("https");
 
-function pwd() {}
+function pwd(print) {
+  print = process.cwd();
+}
 
-function date() {}
+function date(print) {
+  print(new Date().toString());
+}
 
-function echo() {}
+function echo(print, args) {
+  print(args);
+}
 
-function ls() {}
+function ls(print) {
+  fs.readdir(".", (error, files) => {
+    if (error) {
+      throw error;
+    }
+    let filesString = files.join(" ");
+    print(filesString);
+  });
+}
 
-function cat() {}
+function cat(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) {
+      throw error;
+    }
+    print(data);
+  });
+}
 
-function head() {}
+function head(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) {
+      throw error;
+    }
+    let linesData = data.split("\n")[0];
+    print(linesData);
+  });
+}
 
-function tail() {}
+function tail(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) {
+      throw error;
+    }
+    let linesData = data.split("\n");
+    let lastLineData = linesData[linesData.length - 1].trim();
+    print(lastLineData);
+  });
+}
 
-function curl() {}
+function curl(print, path) {
+  utils.request(path, (error, data) => {
+    if (error) {
+      throw new Error(error);
+    }
+    print(data);
+  });
+}
 
-module.exports = {};
+module.exports = {
+  pwd,
+  tail,
+  date,
+  curl,
+  head,
+  cat,
+  ls,
+  echo,
+};
